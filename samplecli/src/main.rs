70972ca35e833802ea5131927ee59539ba3f9993
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-07-05 22:00:13 +0900

    deriveパターンのプログラムに変更

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,37 +1,29 @@
 // ライブラリをインポートする。
-use clap::{App, Arg};
+use clap::Clap;
+use std::fs::File;
+use std::io::{ BufRead, BufReader };
+
+#[derive(Clap, Debug)]
+#[clap(
+    name= "My RPN program",
+    version = "1.0.0",
+    author = "Haruki Kondo",
+    about = "Super swesome sample RPN calculator"
+)]
+
+// 構造体Opts
+struct Opts {
+    // sets the level of vervosity
+    #[clap(short, long)]
+    verbose: bool,
+    // Formula written in RPN
+    #[clap(name = "FILE")]
+    furmula_file: Option<String>,
+}
 
 /**
  * メイン関数
  */
 fn main() {
-    // Appインスタンスを作成する。
-    let matches = App::new("My RPN program")
-                        .version("1.0.0")
-                        .author("Haruki Kondo")
-                        .about("Super awesome sample RPN calculator")
-                        .arg(
-                            Arg::new("formula_file")
-                                .about("Formulas written in RPN")
-                                .value_name("FILE")
-                                .index(1)
-                                .required(false),
-                        )
-                        .arg(
-                            Arg::new("verbose")
-                                .about("Sets the level of verbosity")
-                                .short('v')
-                                .long("verbose")
-                                .required(false),
-                        )
-                        .get_matches();
-
-    // パターンにマッチしているかチェックする
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
-    }
-
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified? :{}", verbose);
+    
 }
