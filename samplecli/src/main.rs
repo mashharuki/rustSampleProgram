1b630fc2091c7871d902ed4cefcc7bc6a35f736c
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-07-10 10:49:42 +0900

    main.rsにanyhowを適用させました。

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,7 +1,9 @@
 // ライブラリをインポートする。
 use clap::Clap;
 use std::fs::File;
+use std::path::PathBuf;
 use std::io::{ BufRead, BufReader, stdin };
+use anyhow::{ bail, ensure, Context, Result };
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -18,7 +20,7 @@ struct Opts {
     verbose: bool,
     // Formula written in RPN
     #[clap(name = "FILE")]
-    furmula_file: Option<String>,
+    furmula_file: Option<PathBuf>,
 }
 
 // 構造体RpnCalculator
@@ -30,23 +32,27 @@ impl RpnCalculator {
         Self(verbose)
     }
     // 計算関数
-    pub fn eval(&self, formula: &str) -> i32 {
+    pub fn eval(&self, formula: &str) -> Result<i32> {
         // 空白で区切る
         let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
         // 内部計算用の関数を呼び出す
         self.eval_inner(&mut tokens)
     }
     // 内部計算関数
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
         // Vec型のインスタンスを生成
         let mut stack = Vec::new();
+        // エラーが起きた場所を特定するための変数
+        let mut pos = 0; 
 
         while let Some(token) = tokens.pop() {
+            // 加算する。
+            pos += 1;
             if let Ok(x) = token.parse::<i32>() {
                 stack.push(x);
             } else {
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
                 // 四則演算用の記号であるかチェックする。
                 let res = match token {
                     "+" => x + y,
@@ -54,7 +60,7 @@ impl RpnCalculator {
                     "*" => x * y,
                     "/" => x / y,
                     "%" => x % y,
-                    _ => panic!("invalid token"),
+                    _ => bail!("invalid token at {}", pos),
                 };
                 stack.push(res);
             }
@@ -63,48 +69,49 @@ impl RpnCalculator {
                 println!("{:?} {:?}", tokens, stack);
             }
         }
-        if stack.len() == 1{
-            stack[0]
-        } else {
-            panic!("invalid syntax")
-        }
+        // スタックの要素数が1以外ならエラー
+        ensure!(stack.len() == 1, "invalid syntax");
+        Ok(stack[0])
     }
 }
 
 /**
  * メイン関数
  */
-fn main() {
+fn main() -> Result<()> {
     let opts = Opts::parse();
     // 計算ロジックを追加する。
     if let Some(path) = opts.furmula_file {
         // ファイル名を取得する
-        let f = File::open(path).unwrap();
+        let f = File::open(path)?;
         let reader = BufReader::new(f);
         // run 関数を呼び出す。
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     } else {
         // 標準入力に対応する
         let stdin = stdin();
         // 入力をロックする。
         let reader = stdin.lock();
         // run関数を呼び出す。
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     }
 }
 
 /**
  * run関数
  */
-fn run<R: BufRead>(reader: R, verbose: bool) {
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
     // RpnCalculator型のインスタンスを生成
     let calc = RpnCalculator::new(verbose);
     // 1行ずつファイルの内容を読み込む
     for line in reader.lines() {
-        let line = line.unwrap();
-        let answer = calc.eval(&line);
-        println!("{}", line);
+        let line = line?;
+        match calc.eval(&line) {
+            Ok(answer) => println!("{}", answer),
+            Err(e) => eprintln!("{:#?}", e),
+        }
     }
+    Ok(())
 }
 
 /**
@@ -118,15 +125,15 @@ mod tests {
     fn test_ok() {
         // RpnCalculator型の変数を用意する。
         let calc = RpnCalculator::new(false);
-        assert_eq!(calc.eval("5"), 5);
-        assert_eq!(calc.eval("50"), 50);
-        assert_eq!(calc.eval("-50"), -50);
+        assert_eq!(calc.eval("5").unwrap(), 5);
+        assert_eq!(calc.eval("50").unwrap(), 50);
+        assert_eq!(calc.eval("-50").unwrap(), -50);
         // 四則演算のテスト
-        assert_eq!(calc.eval("2 3 +"), 5);
-        assert_eq!(calc.eval("2 3 *"), 6);
-        assert_eq!(calc.eval("2 3 -"), -1);
-        assert_eq!(calc.eval("2 3 /"), 0);
-        assert_eq!(calc.eval("2 3 %"), 2);
+        assert_eq!(calc.eval("2 3 +").unwrap(), 5);
+        assert_eq!(calc.eval("2 3 *").unwrap(), 6);
+        assert_eq!(calc.eval("2 3 -").unwrap(), -1);
+        assert_eq!(calc.eval("2 3 /").unwrap(), 0);
+        assert_eq!(calc.eval("2 3 %").unwrap(), 2);
     }
 
     #[test]
@@ -134,6 +141,8 @@ mod tests {
     fn test_ng() {
         // RpnCalculator型の変数を用意する。
         let calc = RpnCalculator::new(false);
-        calc.eval("1 1 ^");
+        assert!(calc.eval("").is_err());
+        assert!(calc.eval("1 1 1 +").is_err());
+        assert!(calc.eval("+ 1 1").is_err());
     }
 }
